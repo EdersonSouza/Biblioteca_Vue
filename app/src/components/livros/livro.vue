efe7ea1f0af5209cbd2fd53e9c44051b2571cb1f
edersonSouza <ederson.souza.mg@gmail.com>
2020-06-15 16:22:19 -0400

    requisição

--- app/src/components/livros/livro.vue
+++ app/src/components/livros/livro.vue
@@ -1,94 +1,21 @@
 <template>
-    <div>
-        <v-btn
-      bottom
-      color="#009688"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Cadastrar Livro
-        </v-card-title>
-        <v-container>
-          <v-row class="mx-2">
-            <v-col
-              class="align-center justify-space-between"
-              cols="12"
-            >
-              <v-row
-                align="center"
-                class="mr-0"
-              >
-                <v-text-field
-                  placeholder="Nome"
-                ></v-text-field>
-              </v-row>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                placeholder="Nome do autor"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="mdi-phone"
-                placeholder="(000) 000 - 0000"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-text"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-btn
-            text
-            color="primary"
-          >More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >Cancel</v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    </div>
+  <div>
+    <list></list>
+    <create></create>
+  </div>
 </template>
 <script>
+
+import create from './createLivros'
+import list from './listLivros'
+
 export default {
+  components: {
+    create,
+    list
+},
     data: () => ({
-      dialog: false,
+     
       
     }),
     
