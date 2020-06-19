ebf7d20195235850f89141e87ac87682c6706749
edersonSouza <ederson.souza.mg@gmail.com>
2020-06-19 14:11:19 -0400

    estilizando tabela

--- app/src/components/livros/listLivros.vue
+++ app/src/components/livros/listLivros.vue
@@ -5,7 +5,7 @@
       :search="search"
       :sort-desc="sortDesc"
       hide-default-footer
-      
+
     >
       <template v-slot:header>
         <v-toolbar
@@ -35,21 +35,19 @@
               :items-per-page="itemsPerPage"
               class="elevation-1"
               @page-count="pageCount = $event"
-               hide-default-footer
               ></v-data-table>
+               <v-text-field
+                  :value="itemsPerPage"
+                  label="Items por pagina"
+                  type="number"
+                  min="-1"
+                  max="15"
+                  @input="itemsPerPage = parseInt($event, 10)"
+                ></v-text-field>
+                <v-pagination v-model="page" :length="pageCount"></v-pagination>
             </v-card>
         
-           <div class="text-center pt-2" >
-          <v-text-field
-            :value="itemsPerPage"
-            label="Items por pagina"
-            type="number"
-            min="-1"
-            max="15"
-            @input="itemsPerPage = parseInt($event, 10)"
-          ></v-text-field>
-          <v-pagination v-model="page" :length="pageCount"></v-pagination>
-        </div>
+        
       </template>
     </v-data-iterator>
   </v-container>
