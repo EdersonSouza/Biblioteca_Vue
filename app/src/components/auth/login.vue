e6e7123ce49a0f54d45c82cb3623d1ea18f3ded5
edersonSouza <ederson.souza.mg@gmail.com>
2020-07-21 16:19:01 -0400

    loading

--- app/src/components/auth/login.vue
+++ app/src/components/auth/login.vue
@@ -46,19 +46,49 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="#009688" @click="login">Login</v-btn>
+                <v-btn 
+                  :disabled="dialog"
+                  :loading="dialog"
+                  color="#009688" 
+                  @click="login">Login</v-btn>
               </v-card-actions>
+              
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-content>
+      <template>
+      <div class="text-center">
+        <v-dialog
+          v-model="dialog"
+          hide-overlay
+          persistent
+          width="300"
+        >
+          <v-card
+            color="primary"
+            dark
+          >
+            <v-card-text>
+              Buscando Dados
+              <v-progress-linear
+                indeterminate
+                color="white"
+                class="mb-0"
+              ></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
   </v-app>
 </template>
 
 <script>
   export default {
       data:()=> ({
+        dialog: false,
         user:{
           nomeUser:'',
           password:''
@@ -66,9 +96,17 @@
         erro:''
 
       }),
+      watch: {
+          dialog (val) {
+            if (!val) return
+
+            setTimeout(() => (this.dialog = false), 5000)
+          },
+      },
 
       methods: {
           login (){
+              this.dialog=true
               this.$store.dispatch('login',this.user)
               .then(()=>{
                 this.$router.push({name:'livros'});
